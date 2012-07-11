07b21ccc80150f2bb73c2ecb270f76458cfb4e91
Christian Köstlin <christian.koestlin@esrlabs.com>
2012-07-11 18:23:39 +0200

    added cxx wizard to the project
    Change-Id: I160808d40e72dbd480ed4517a81e9eb4071dcac8

--- gemspec.rb
+++ gemspec.rb
@@ -1,15 +1,17 @@
-GEM_VERSION = "0.1.0"
-
 spec = Gem::Specification.new do |s|
-  s.name = 'rubydsl'
+  s.name = 'cxxprojectrubydsl'
   s.description = 'ruby-based dsl for cxxproject'
   s.summary = 'defines the method cxx_configuration'
   s.version = '0.1.0'
   s.author = 'christian koestlin'
   s.email = 'christian.koestlin@gmail.com'
   s.platform = Gem::Platform::RUBY
-  s.files = FileList['lib/**/*.rb'].to_a
+  s.files = FileList['lib/**/*.rb', 'lib/**/*.template'].to_a
   s.require_path = 'lib'
   s.add_dependency 'cxxproject'
+  s.add_dependency 'highline'
+
+  s.executables = ['cxx']
+
   s.has_rdoc = false
 end
