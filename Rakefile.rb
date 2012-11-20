8dc936741ffbb09c6e9341f071f8ff47ee2af383
Christian Köstlin <christian.koestlin@esrlabs.com>
2012-11-20 21:49:15 +0100

    changed to new test-strategy
    Change-Id: I7dc4d9b616818a782f60d627c8b982524b365f1f

--- Rakefile.rb
+++ Rakefile.rb
@@ -7,27 +7,52 @@ projects = ['frazzle',
             'cxxproject',
             'cxxproject_gcctoolchain']
 
-desc 'cleanup all built gems'
-task :clean do
-  projects.each do |p|
-    cd "../#{p}" do
-      sh 'rm -rf pkg'
-    end
-  end
+def gems
+  ['frazzle', 'cxxproject', 'cxx', 'cxxproject_gcctoolchain']
 end
 
-desc 'install prerequisites for build'
-task :wipe_gems do
-  sh "rvm --force gemset empty"
-end
-
-desc 'install all built gems'
-task :build_and_install_gems do
-  projects.each do |p|
-    cd "../#{p}" do
+desc 'prepare acceptance tests'
+task :prepare_accept do
+  gems.each do |gem|
+    cd "../#{gem}" do
       sh 'rm -rf pkg'
       sh 'rake package'
-      sh 'rake install'
     end
   end
+  gems.each do |gem|
+    sh "gem install ../#{gem}/pkg/*.gem"
+  end
 end
+
+desc 'run acceptance tests'
+RSpec::Core::RakeTask.new(:accept) do |t|
+  t.pattern = 'accept/**/*_spec.rb'
+#  if ENV['BUILD_SERVER']
+#    t.rspec_opts = '-r ./junit.rb -f JUnit -o build/test_details.xml'
+#  end
+end
+#
+#desc 'cleanup all built gems'
+#task :clean do
+#  projects.each do |p|
+#    cd "../#{p}" do
+#      sh 'rm -rf pkg'
+#    end
+#  end
+#end
+#
+#desc 'install prerequisites for build'
+#task :wipe_gems do
+#  sh "rvm --force gemset empty"
+#end
+#
+#desc 'install all built gems'
+#task :build_and_install_gems do
+#  projects.each do |p|
+#    cd "../#{p}" do
+#      sh 'rm -rf pkg'
+#      sh 'rake package'
+#      sh 'rake install'
+#    end
+#  end
+#end
